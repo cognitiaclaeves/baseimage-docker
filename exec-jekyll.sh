1202cef2be3eebc1ed96fcd1c6a8b2831b2c85fe
Cognitiaclaeves <cognitiaclaeves@gmail.com>
2016-03-08 22:17:43 -0600

    Updated exec-jekyll.sh to create default jekyll environment if /data dir is uninitialized.

--- exec-jekyll.sh
+++ exec-jekyll.sh
@@ -7,24 +7,25 @@ docker rm -v jekyll_runtime 2> /dev/null
 drafts=''
 
 
-if [ ! -f /vagrant/home/data/ ];then
+if [ ! -f /home/vagrant/data/_config.yml ];then
 	echo "Initializing Jekyll Environment!"
 
-	cd /vagrant/home/data
+	cd /home/vagrant/data
 	docker run \
 		--label=jekyll \
 		--publish 4000:4000 \
-		--rm \ 
-		--volume=$(pwd):/srv/jekyll 
+		--rm \
+		--volume="$(pwd):/srv/jekyll" \
 		jekyll/jekyll:pages jekyll new . --force
 fi
-
 
 if [ -f '/vagrant/drafts' ]; then
   drafts='--drafts'
 fi
 
+echo "Running Jekyll!"
 
+cd /home/vagrant/data
 docker run \
 	--env FORCE_POLLING=true \
 	--env JEKYLL_ENV=development \
